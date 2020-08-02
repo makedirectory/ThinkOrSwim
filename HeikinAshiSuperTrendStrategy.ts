dd66409c475ea0403371f808df748cc0fb2d9b4f
Andrew Schwartz <andrew@mk-dir.com>
2020-08-01 22:14:28 -0400

    updates

--- HeikinAshiSuperTrendStrategy.ts
+++ HeikinAshiSuperTrendStrategy.ts
@@ -11,21 +11,21 @@ input aggrPd = AggregationPeriod.FOUR_HOURS;
 
 # Heikin Ashi Stuff
 # Working off of HA open, high, low, close
-def hacl1 = ( open(period = aggrPd) + high(period = aggrPd) + low(period = aggrPd) + close(period = aggrPd) ) / 4;
-def haop1 = CompoundValue(1, (haop1[1] + hacl1[1]) / 2, (open(period = aggrPd)[1] + close(period = aggrPd)[1]) / 2);
-def hahi1 = Max( open(period = aggrPd), Max( hacl1, haop1 ) );
-def halo1 = Min( low(period = aggrPd), Min( hacl1, haop1 ) );
-def hahl2_1 = (hahi1 + halo1) / 2;
+def hacl = ( open(period = aggrPd) + high(period = aggrPd) + low(period = aggrPd) + close(period = aggrPd) ) / 4;
+def haop = CompoundValue(1, (haop[1] + hacl[1]) / 2, (open(period = aggrPd)[1] + close(period = aggrPd)[1]) / 2);
+def hahi = Max( open(period = aggrPd), Max( hacl, haop ) );
+def halo = Min( low(period = aggrPd), Min( hacl, haop ) );
+def hahl2 = (hahi + halo) / 2;
 
-def iATR1 = MovingAverage(AverageType.WILDERS, TrueRange(hahi1, hacl1, halo1), ST_Period);
+def iATR = MovingAverage(AverageType.WILDERS, TrueRange(hahi, hacl, halo), ST_Period);
 
-def tmpUp1 = hahl2_1 - (ST_Coeff * iATR1);
-def tmpDn1 = hahl2_1 + (ST_Coeff * iATR1);
-def finalUp1 = If(close(period = aggrPd)[1] > finalUp1[1], Max(tmpUp1, finalUp1[1]), tmpUp1);
-def finalDn1 = If(close(period = aggrPd)[1] < finalDn1[1], Min(tmpDn1, finalDn1[1]), tmpDn1);
-def trendDir1 = If( close(period = aggrPd) > finalDn1[1], 1, If( close(period = aggrPd) < finalUp1[1], -1, If(!IsNaN(trendDir1[1]), trendDir1[1], 1) ) );
-def trendLine1 = If(trendDir1 == 1, finalUp1, finalDn1);
-plot SuperTrend = trendLine1;
+def tmpUp = hahl2 - (ST_Coeff * iATR);
+def tmpDn = hahl2 + (ST_Coeff * iATR);
+def finalUp = If(close(period = aggrPd)[1] > finalUp[1], Max(tmpUp, finalUp[1]), tmpUp);
+def finalDn = If(close(period = aggrPd)[1] < finalDn[1], Min(tmpDn, finalDn[1]), tmpDn);
+def trendDir = If( close(period = aggrPd) > finalDn[1], 1, If( close(period = aggrPd) < finalUp[1], -1, If(!IsNaN(trendDir[1]), trendDir[1], 1) ) );
+def trendLine = If(trendDir == 1, finalUp, finalDn);
+plot SuperTrend = trendLine;
 
 SuperTrend.DefineColor( "up", Color.GREEN );
 SuperTrend.DefineColor( "dn", Color.RED );
@@ -41,7 +41,7 @@ def entryPrice = open(period = aggrPd)[-1];
 def exitPrice = open(period = aggrPd)[-1];
 def tSize = Round(Average(tradeFunds / entryPrice));
 
-def bullmk = ( close(period = aggrPd)[-1] > SuperTrend[-1] and close(period = aggrPd)[0] < SuperTrend[0] ) ;
+def bullmk = ( close(period = aggrPd)[-1] > SuperTrend[-1] and close(period = aggrPd)[0] < SuperTrend[0] );
 def bearmk = ( close(period = aggrPd)[-1] < SuperTrend[-1] and close(period = aggrPd)[0] > SuperTrend[0] );
 
 # Bull Market BUY
