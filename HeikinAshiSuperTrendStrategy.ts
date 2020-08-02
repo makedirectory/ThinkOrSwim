2957f2dab4559d1c11241042aea01d4f7cc702d8
Andrew Schwartz <andrew@mk-dir.com>
2020-08-01 22:11:21 -0400

    simplify

--- HeikinAshiSuperTrendStrategy.ts
+++ HeikinAshiSuperTrendStrategy.ts
@@ -2,84 +2,47 @@ declare upper;
 input ST_Coeff = 1; # Between 1 - 100
 input ST_Period = 15; # Between 1 - 100
 input tradeFunds = 10000; # Int or Percent of Account Value
-input calulatedMovingAverage = 200; # Moving Average used in Strategy
+input calulatedMovingAverage = 200; # Days in Moving Average
+input aggrPd = AggregationPeriod.FOUR_HOURS;
 
 ###############
-# SuperTrend1 #
+# SuperTrend #
 ###############
-input aggrPd1 = AggregationPeriod.FOUR_HOURS;
 
 # Heikin Ashi Stuff
 # Working off of HA open, high, low, close
-def hacl1 = ( open(period = aggrPd1) + high(period = aggrPd1) + low(period = aggrPd1) + close(period = aggrPd1) ) / 4;
-def haop1 = CompoundValue(1, (haop1[1] + hacl1[1]) / 2, (open(period = aggrPd1)[1] + close(period = aggrPd1)[1]) / 2);
-def hahi1 = Max( open(period = aggrPd1), Max( hacl1, haop1 ) );
-def halo1 = Min( low(period = aggrPd1), Min( hacl1, haop1 ) );
+def hacl1 = ( open(period = aggrPd) + high(period = aggrPd) + low(period = aggrPd) + close(period = aggrPd) ) / 4;
+def haop1 = CompoundValue(1, (haop1[1] + hacl1[1]) / 2, (open(period = aggrPd)[1] + close(period = aggrPd)[1]) / 2);
+def hahi1 = Max( open(period = aggrPd), Max( hacl1, haop1 ) );
+def halo1 = Min( low(period = aggrPd), Min( hacl1, haop1 ) );
 def hahl2_1 = (hahi1 + halo1) / 2;
 
-#def iATR1 = ATR(ST_Period);
 def iATR1 = MovingAverage(AverageType.WILDERS, TrueRange(hahi1, hacl1, halo1), ST_Period);
 
-def tmpUp1 = hahl2_1 - (ST_Coeff * iATR1); #hahl2 is better than hl2
-def tmpDn1 = hahl2_1 + (ST_Coeff * iATR1); #hahl2 is better than hl2
-def finalUp1 = If(close(period = aggrPd1)[1] > finalUp1[1], Max(tmpUp1, finalUp1[1]), tmpUp1);
-def finalDn1 = If(close(period = aggrPd1)[1] < finalDn1[1], Min(tmpDn1, finalDn1[1]), tmpDn1);
-def trendDir1 = If( close(period = aggrPd1) > finalDn1[1], 1, If( close(period = aggrPd1) < finalUp1[1], -1, If(!IsNaN(trendDir1[1]), trendDir1[1], 1) ) );
+def tmpUp1 = hahl2_1 - (ST_Coeff * iATR1);
+def tmpDn1 = hahl2_1 + (ST_Coeff * iATR1);
+def finalUp1 = If(close(period = aggrPd)[1] > finalUp1[1], Max(tmpUp1, finalUp1[1]), tmpUp1);
+def finalDn1 = If(close(period = aggrPd)[1] < finalDn1[1], Min(tmpDn1, finalDn1[1]), tmpDn1);
+def trendDir1 = If( close(period = aggrPd) > finalDn1[1], 1, If( close(period = aggrPd) < finalUp1[1], -1, If(!IsNaN(trendDir1[1]), trendDir1[1], 1) ) );
 def trendLine1 = If(trendDir1 == 1, finalUp1, finalDn1);
-plot SuperTrend1 = trendLine1;
+plot SuperTrend = trendLine1;
 
-SuperTrend1.DefineColor( "up", Color.GREEN );
-SuperTrend1.DefineColor( "dn", Color.RED );
-SuperTrend1.AssignValueColor(SuperTrend1.Color("up"));
-SuperTrend1.AssignValueColor( if close(period = aggrPd1)[1] > SuperTrend1[1] then SuperTrend1.Color( "up" ) else SuperTrend1.Color( "dn" ) );
-SuperTrend1.SetLineWeight( 2 );
-
-###############
-# SuperTrend2 #
-###############
-input aggrPd2 = AggregationPeriod.DAY;
-
-# Heikin Ashi Stuff
-# Working off of HA open, high, low, close
-def hacl2 = ( open(period = aggrPd2) + high(period = aggrPd2) + low(period = aggrPd2) + close(period = aggrPd2) ) / 4;
-def haop2 = CompoundValue(1, (haop2[1] + hacl2[1]) / 2, (open(period = aggrPd2)[1] + close(period = aggrPd2)[1]) / 2);
-def hahi2 = Max( open(period = aggrPd2), Max( hacl2, haop2 ) );
-def halo2 = Min( low(period = aggrPd2), Min( hacl2, haop2 ) );
-def hahl2_2 = (hahi2 + halo2) / 2;
-
-#def iATR1 = ATR(ST_Period);
-def iATR2 = MovingAverage(AverageType.WILDERS, TrueRange(hahi2, hacl2, halo2), ST_Period);
-
-def tmpUp2 = hahl2_2 - (ST_Coeff * iATR2); #hahl2 is better than hl2
-def tmpDn2 = hahl2_2 + (ST_Coeff * iATR2); #hahl2 is better than hl2
-def finalUp2 = If(close(period = aggrPd2)[1] > finalUp2[1], Max(tmpUp2, finalUp2[1]), tmpUp2);
-def finalDn2 = If(close(period = aggrPd2)[1] < finalDn2[1], Min(tmpDn2, finalDn2[1]), tmpDn2);
-def trendDir2 = If( close(period = aggrPd2) > finalDn2[1], 1, If( close(period = aggrPd2) < finalUp2[1], -1, If(!IsNaN(trendDir2[1]), trendDir2[1], 1) ) );
-def trendLine2 = If(trendDir2 == 1, finalUp2, finalDn2);
-plot SuperTrend2 = trendLine2;
-
-SuperTrend2.DefineColor( "up", Color.GREEN );
-SuperTrend2.DefineColor( "dn", Color.RED );
-SuperTrend2.AssignValueColor(SuperTrend2.Color("up"));
-SuperTrend2.AssignValueColor( if close(period = aggrPd2)[1] > SuperTrend2[1] then SuperTrend2.Color( "up" ) else SuperTrend2.Color( "dn" ) );
-SuperTrend2.SetLineWeight( 2 );
+SuperTrend.DefineColor( "up", Color.GREEN );
+SuperTrend.DefineColor( "dn", Color.RED );
+SuperTrend.AssignValueColor(SuperTrend.Color("up"));
+SuperTrend.AssignValueColor( if close(period = aggrPd)[1] > SuperTrend[1] then SuperTrend.Color( "up" ) else SuperTrend.Color( "dn" ) );
+SuperTrend.SetLineWeight( 2 );
 
 ###############
-# SuperTrend3 #
+# Strategy Calculations #
 ###############
 
-
-# Strategy Calculations
-def entryPrice = open(period = aggrPd1)[-1];
-def exitPrice = open(period = aggrPd1)[-1];
+def entryPrice = open(period = aggrPd)[-1];
+def exitPrice = open(period = aggrPd)[-1];
 def tSize = Round(Average(tradeFunds / entryPrice));
 
-def sma = MovingAverage(AverageType.SIMPLE, close(period = aggrPd1), calulatedMovingAverage);
-def bullMarket = open(period = aggrPd1)[-1] > sma;
-def bearMarket = open(period = aggrPd1)[-1] < sma;
-
-def bullmk = ( close(period = aggrPd1)[-1] > SuperTrend1[-1] and close(period = aggrPd1)[0] < SuperTrend1[0] ) ;
-def bearmk = ( close(period = aggrPd1)[-1] < SuperTrend1[-1] and close(period = aggrPd1)[0] > SuperTrend1[0] );
+def bullmk = ( close(period = aggrPd)[-1] > SuperTrend[-1] and close(period = aggrPd)[0] < SuperTrend[0] ) ;
+def bearmk = ( close(period = aggrPd)[-1] < SuperTrend[-1] and close(period = aggrPd)[0] > SuperTrend[0] );
 
 # Bull Market BUY
 AddOrder(OrderType.BUY_TO_OPEN, bullmk, price = entryPrice, tradeSize = tSize, tickcolor = GetColor(0), arrowcolor = GetColor(1));
